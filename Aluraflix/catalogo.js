5e9ee34da6e7ca5cf6123e37bedbd3435c6370af
Mariana Borges Machado de Andrade <118206264+mariborges22@users.noreply.github.com>
2023-02-01 13:17:53 -0300

    Create catalogo.js

---
+++ Aluraflix/catalogo.js
@@ -0,0 +1,41 @@
+var animesMari=[
+  "https://upload.wikimedia.org/wikipedia/en/3/32/Serial_Experiments_Lain_DVD_vol_1.jpg",
+ 
+  "https://1.bp.blogspot.com/-R2jOY0_eZXY/VoRwaSR0ZEI/AAAAAAAAA5M/Znue4C7AaCc/s1600/The-Perfect-Insider.jpg",
+  
+  "https://beladistopia.com/wp-content/uploads/2021/05/57150d15b50015e6394781ba7fa5dd7a-1.jpg",
+   
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ85qt_fBl5Wgsux3SyvPd7afC72e9aKAc1iw&usqp=CAU",
+  "https://image.tmdb.org/t/p/w600_and_h900_bestv2/uF11jqAUCXJzG5aLihc8My4IIgc.jpg",
+  "https://cdn.culturagenial.com/imagens/death-note-cartaz.jpg",
+ 
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRDGtbUCeAdibKUXuen52jbCqTp1YsE0nKQg&usqp=CAU",
+  
+  "https://m.media-amazon.com/images/I/51r79YForqL._AC_.jpg",
+   "https://images.justwatch.com/poster/261879046/s718/nogemu-noraihu.%7Bformat%7D",
+ 
+  "https://www.intoxianime.com/wp-content/uploads/2021/11/tomodachigame_teaser.jpg",
+ "https://www.crunchyroll.com/imgsrv/display/thumbnail/640x360/catalog/crunchyroll/5bc8f71d913ed8604bc3cc6db65a0c2c.jpe",
+ 
+  "https://a-static.mlcdn.com.br/800x560/livro-mieruko-chan-vol-5/livrdavila/819492/84df3acf2b658fa1e960f46920bf3614.jpg",
+ 
+  "https://m.media-amazon.com/images/M/MV5BMmMzOWNhNTYtYmY0My00OGJiLWIzNDUtZWRhNGY0NWFjNzFmXkEyXkFqcGdeQXVyNjUxMDQ0MTg@._V1_.jpg",
+                "https://cdn.myanimelist.net/images/about_me/ranking_items/7165131-9d0344d6-cd32-4f54-91fd-fd74a99c0c02.jpg?t=1661787028",
+  
+ "https://m.media-amazon.com/images/I/91ifr0L+XrL.jpg",
+ ,
+  "https://cdn.myanimelist.net/images/anime/1301/93586.jpg",
+                "https://upload.wikimedia.org/wikipedia/pt/7/7f/Kimi-no-Na-wa-poster.jpg",
+                "https://upload.wikimedia.org/wikipedia/pt/1/1a/Mirainikkicover1.jpg",
+                "https://alternativanerd.com.br/wp-content/uploads/2019/12/classroom-of-the-elite.jpg",
+                "https://www.crunchyroll.com/imgsrv/display/thumbnail/640x360/catalog/crunchyroll/dbb7e4ac3442551634a2e80fa54106cc.jpe",
+                "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/a3c042b2aad7d2d742af84dfd223e04b.jpe",
+                "https://animenh.files.wordpress.com/2012/12/another_anime.jpg "];
+
+var animesnome=["Serial Experiments Lain","Subete Ga F ni Naru","Joker Game","Violet Evergaden","Hunter Hunter 2011","Akatsuki no Yona","Shinigami Bocchan ro Kuro Maid","Fullmetal Achemist ","No game no life","Tomodachi game","Mieruko-chan","Perfect Blue","Mahou Shoujo Madoka Magica","Shumatsuu no Walkire","Bunny girl senpai","Classroom of the Elite","Plunderer","One Punch Man","Charlotte","Kimi no Wa","Mirai Nikki","Free","Spy Family","Death Parade","Another"];
+for(var r=0;r<animesnome.length;r++){
+    document.write('<img src='+ animesnome[r]+ '>')}
+
+  for(var i=0;i<animesMari.length;i++){
+    document.write('<img src='+ animesMari[i] + '>');
+}
